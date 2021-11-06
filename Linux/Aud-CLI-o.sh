309a58dfc9fcca038babe52fbda71a5b9dc67c19
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-11-06 05:38:49 +0000

    Update Aud-CLI-o v. 1.1
    Aud-CLI-o v. 1.1:
    
    - IF-Abfrage integriert, welche prüft ob youtube-dl installiert ist. Falls es installiert ist, macht Aud-CLI-o wie gewohnt weiter. Ist youtube-dl hingegen nicht insalliert, wird dies nachgeholt.
    
    Sonstiges:
    
    - Downloadverzeichnis gestzt (DOWNDIR).

--- Linux/Aud-CLI-o.sh
+++ Linux/Aud-CLI-o.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo ---------------------------------------------------
-echo Aud-CLI-o v.1.0 20211009 by Monstanner
-echo ---------------------------------------------------
-
-cd #Hier bitte den Pfad eingeben, wo das Lied gespeichert werden soll.
+if which youtube-dl >/dev/null;
+then echo "youtube-dl ist installiert." &>/dev/null
+else echo "youtube-dl ist nicht installiert. Um Aud-CLI-o verwenden zu können, muss youtube-dl installiert werden." && xterm -e "sudo zypper ln  -l -y youtube-dl"
+fi
 
 #!/bin/bash
+
+echo ---------------------------------------------------
+echo Aud-CLI-o v.1.1 20211106 by Monstanner
+echo ---------------------------------------------------
+
+#!/bin/bash
+
+DOWNDIR=$HOME/Musik/Heruntergeladen/
+mkdir $DOWNDIR 2> /dev/null
 
 PS3='In welchen Format soll das Lied heruntergeladen werden? '
 options=("MP3 Download" "AAC Download" "FLAC Download" "M4A Download" "OPUS Download" "VORBIS Download" "WAV Download" "Über" "Beenden")
@@ -20,7 +28,7 @@ do
               while read INPUT
                 do
                   echo Wird heruntergeladen...
-                  youtube-dl -xo "%(title)s.%(ext)s" --audio-format mp3 --audio-quality 0 "$INPUT"
+                  youtube-dl -xo "$DOWNDIR%(title)s.%(ext)s" --audio-format mp3 --audio-quality 0 "$INPUT"
               break
               done
               exit
@@ -32,7 +40,7 @@ do
               while read INPUT
                 do
                 echo Wird heruntergeladen...
-                youtube-dl -xo "%(title)s.%(ext)s" --audio-format aac --audio-quality 0 "$INPUT"
+                youtube-dl -xo "$DOWNDIR%(title)s.%(ext)s" --audio-format aac --audio-quality 0 "$INPUT"
               break
               done
               exit
@@ -44,7 +52,7 @@ do
               while read INPUT
                 do
                 echo Wird heruntergeladen...
-                youtube-dl -xo "%(title)s.%(ext)s" --audio-format flac --audio-quality 0 "$INPUT"
+                youtube-dl -xo "$DOWNDIR%(title)s.%(ext)s" --audio-format flac --audio-quality 0 "$INPUT"
               break
               done
               exit
@@ -56,7 +64,7 @@ do
               while read INPUT
                 do
                 echo Wird heruntergeladen...
-                youtube-dl -xo "%(title)s.%(ext)s" --audio-format m4a --audio-quality 0 "$INPUT"
+                youtube-dl -xo "$DOWNDIR%(title)s.%(ext)s" --audio-format m4a --audio-quality 0 "$INPUT"
               break
               done
               exit
@@ -68,7 +76,7 @@ do
               while read INPUT
                 do
                 echo Wird heruntergeladen...
-                youtube-dl -xo "%(title)s.%(ext)s" --audio-format opus --audio-quality 0 "$INPUT"
+                youtube-dl -xo "$DOWNDIR%(title)s.%(ext)s" --audio-format opus --audio-quality 0 "$INPUT"
               break
               done
               exit
@@ -80,7 +88,7 @@ do
               while read INPUT
                 do
                 echo Wird heruntergeladen...
-                youtube-dl -xo "%(title)s.%(ext)s" --audio-format vorbis --audio-quality 0 "$INPUT"
+                youtube-dl -xo "$DOWNDIR%(title)s.%(ext)s" --audio-format vorbis --audio-quality 0 "$INPUT"
               break
               done
               exit
@@ -92,7 +100,7 @@ do
               while read INPUT
                 do
                 echo Wird heruntergeladen...
-                youtube-dl -xo "%(title)s.%(ext)s" --audio-format wav --audio-quality 0 "$INPUT"
+                youtube-dl -xo "$DOWNDIR%(title)s.%(ext)s" --audio-format wav --audio-quality 0 "$INPUT"
               break
               done
               exit
@@ -101,7 +109,7 @@ do
               echo ---------------------------------------------------
               echo Aud-CLI-o ist ein Terminal Skript und wurde von Monstanner erstellt.
               echo Aud-CLI-o gibt es für GNU/Linux und Windows.
-              echo Aktuelle Version: 1.0 20211009
+              echo Aktuelle Version: 1.1 20211106
               echo Link zu Aud-CLI-o: www.github.com/Monstanner/Aud-CLI-o
               echo Fork von YouTube-DL MP3 v.1.2 20210919
               echo ---------------------------------------------------
